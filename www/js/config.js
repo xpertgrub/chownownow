f5a408c28f2ad2e2b3e849b72b882032e1386b69
Xpert Grub <52222510+xpertgrub@users.noreply.github.com>
2019-06-26 02:08:46 +0000

    Add files via upload

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 var krms_config ={		
-	'ApiUrl':"",				
-	'DialogDefaultTitle':"KMRS",	
-	'APIHasKey':"",
+	'ApiUrl':"https://xpertgrub.com/chownownow/mobileapp/api",				
+	'DialogDefaultTitle':"chownownow",	
+	'APIHasKey':"eHBlcnRncnViOmNob3dub3dub3c",
 	'debug': false
 };
