2cd138d697275cd83a0f2234adc822f51da18c42
Xpert Grub <xpertgrub@users.noreply.github.com>
2019-08-06 11:04:45 +0000

    update

--- www/js/tpl.js
+++ www/js/tpl.js
@@ -467,7 +467,7 @@ function customFields(name, placeholder)
 {
 	var htm='';	
 	htm+='<div class="field-wrapper">';
-	  htm+='<input type="text" name="'+name+'"  class="text-input text-input--underbar" placeholder="'+placeholder+'" value="" has_validation data-validation="required" data-validation-error-msg="this field is mandatory!" >';
+	  htm+='<input type="text" name="'+name+'"  class="text-input text-input--underbar" placeholder="'+placeholder+'" value="" has_validation /*data-validation="required" */ data-validation-error-msg="this field is mandatory!" >';
 	htm+='</div>';
 	return htm;
 }
