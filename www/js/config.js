a8eff1bfe1bae6ca6b25b949cc7df90b7130be9f
Xpert Grub <xpertgrub@users.noreply.github.com>
2019-08-06 14:56:09 +0000

    update

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 var krms_config ={		
 	'ApiUrl':"https://www.chownownow.com/mobileapp/api",				
 	'DialogDefaultTitle':"Chow Now Now",	
-	'APIHasKey':"eHBlcnRncnVifGNob3dub3dub3dtb2JpbGU",
+	'APIHasKey':"ehblcnrncnvifgnob3dub3dub3dtb2jpbgu",
 	'debug': false
 };
